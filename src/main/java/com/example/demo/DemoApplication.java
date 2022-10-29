0e3910c5083bde67ec5588224a1c5620f1d26606
admin <kairatbek.shabotoev@gmail.com>
2022-10-30 00:01:22 +0600

    setup cercle ci

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class DemoApplication {
 
+    @Value("${hello}")
+    String hello;
+
+    @Value("${database}")
+    String database;
+
+    @Value("${awsaccesskey}")
+    String awsaccesskey;
+
+    @Value("${awssecretkey}")
+    String awssecretkey;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/")
     public String greeting() {
-        return """
+        return String.format("""
                 <p>Hello</p>
                 <img src= https://www.thoughtco.com/thmb/CoviI4KgInS4AtTkNRtODkZD6BY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-937057490-401a1830296d458e98f62fdc7b6417d1.jpg>
-                """;
+                <p>%s ->(from code) hello</p>
+                <p>%s ->(from code) database</p>
+                <p>%s ->(from code) awsaccesskey</p>
+                <p>%s ->(from code) awssecretkey</p>
+                """, hello, database, awsaccesskey, awssecretkey);
 
     }
 
